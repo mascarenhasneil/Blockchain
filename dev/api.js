859be76c4f067da8200ea70cfba43db522859b87
Neil Mascarenhas <neilpm1995@gmail.com>
2020-06-01 22:40:17 +0530

    installed UUID and Body Parser

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,17 @@
-var express=require('express')
-var app=express()
+const express=require('express')
+const app=express();
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/blockchain',function(req,res){
-    res.send('Hello Blockchain World')
+    res.send('Hello Blockchain World');
 });
 
 app.post('/transaction',function(req,res){
-
+    console.log(req.body);
+    res.send(`The Amount of transaction is ${req.body.amount} bitcoin`);
 });
 
 app.get('/mine',function(req,res){
