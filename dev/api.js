247664958ae8d5280acceb4c9c85cd1e66a9fd7e
Neil Mascarenhas <neilpm1995@gmail.com>
2020-06-01 16:04:51 +0530

    Installed nodemon to simplify restarting servers

--- dev/api.js
+++ dev/api.js
@@ -1,10 +1,23 @@
 var express=require('express')
 var app=express()
 
-app.get('/',function(req,res){
-    res.send('Hello World')
-})
+app.get('/blockchain',function(req,res){
+    res.send('Hello Blockchain World')
+});
 
-app.listen(3000)
+app.post('/transaction',function(req,res){
+
+});
+
+app.get('/mine',function(req,res){
+
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Listening on Port 3000......");
+});
 
 
